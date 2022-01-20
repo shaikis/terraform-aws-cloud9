36322c086178e147b10194736e3a846769075554
Ismails <shaik.urs@gmail.com>
2022-01-20 13:09:18 +0530

    added users

--- cloud9_membership.tf
+++ cloud9_membership.tf
@@ -9,3 +9,32 @@ resource "aws_cloud9_environment_membership" "this" {
     permissions    = "Read Only"
     user_arn       = "${aws_iam_user.this.*.arn}"
 }
+
+resource "aws_iam_account_password_policy" "pass_policy" {
+  minimum_password_length        = 8
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = false
+  allow_users_to_change_password = true
+}
+
+resource "aws_iam_user_policy" "user_policy" {
+  count = length(var.username)
+  name = "new"
+  user = element(var.username,count.index)
+policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}

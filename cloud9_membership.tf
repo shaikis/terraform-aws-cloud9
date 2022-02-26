b6e242b9010053f2ee7a69d781bd526ea4e8873f
Ismail Shaik <60819791+shaikis@users.noreply.github.com>
2022-02-26 15:20:42 +0530

    Update cloud9_membership.tf

--- cloud9_membership.tf
+++ cloud9_membership.tf
@@ -1,8 +1,8 @@
-#resource "aws_iam_user" "this" {
-#    count = "${length(var.username)}"
-#    name = "${element(var.username,count.index )}"
-#}
-#
+resource "aws_iam_user" "this" {
+    count = "${length(var.username)}"
+    name = "${element(var.username,count.index )}"
+}
+:wq#
 resource "aws_cloud9_environment_membership" "this" {
     count = "${length(var.username)}"
     environment_id = aws_cloud9_environment_ec2.this.id

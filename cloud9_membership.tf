e35c6fbe4b8eea7769a18d709e50439db34ca1d2
Ismail Shaik <60819791+shaikis@users.noreply.github.com>
2022-02-26 15:20:18 +0530

    Update cloud9_membership.tf

--- cloud9_membership.tf
+++ cloud9_membership.tf
@@ -3,12 +3,12 @@
 #    name = "${element(var.username,count.index )}"
 #}
 #
-#resource "aws_cloud9_environment_membership" "this" {
-#    count = "${length(var.username)}"
-#    environment_id = aws_cloud9_environment_ec2.this.id
-#    permissions    = "Read Only"
-#    user_arn       = "${aws_iam_user.this.*.arn}"
-#}
+resource "aws_cloud9_environment_membership" "this" {
+    count = "${length(var.username)}"
+    environment_id = aws_cloud9_environment_ec2.this.id
+    permissions    = "Read Only"
+    user_arn       = "${aws_iam_user.this.*.arn}"
+}
 
 resource "aws_iam_account_password_policy" "pass_policy" {
   minimum_password_length        = 8
